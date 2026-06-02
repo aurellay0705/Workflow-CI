d02a5f65c6b807637b0ad69667b354a1d17e330a
aurellay0705 <152011030+aurellay0705@users.noreply.github.com>
2026-06-03 02:12:55 +0700

    Add files via upload

---
+++ MLProject/modelling.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score
+)
+
+
+mlflow.set_experiment("Titanic_Classification")
+
+# Autolog untuk memenuhi Basic
+mlflow.autolog()
+
+
+df = pd.read_csv("titanic_preprocessed.csv")
+
+print("Dataset Shape:", df.shape)
+
+
+X = df.drop("Survived", axis=1)
+y = df["Survived"]
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+print("Train:", X_train.shape)
+print("Test :", X_test.shape)
+
+
+with mlflow.start_run(run_name="RandomForest_Basic"):
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    print("\nHASIL EVALUASI")
+    print("=" * 40)
+    print("Accuracy :", round(accuracy,4))
+    print("Precision:", round(precision,4))
+    print("Recall   :", round(recall,4))
+    print("F1 Score :", round(f1,4))
+    print("=" * 40)
+
+print("\nTraining selesai")
+print("Jalankan: mlflow ui")
